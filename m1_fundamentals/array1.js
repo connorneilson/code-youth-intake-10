710ee02f2ce9c6ed3d2892f54bd55a967eec1ad9
connorneilson <131685529+connorneilson@users.noreply.github.com>
2023-05-24 10:33:27 -0400

    marks array

---
+++ m1_fundamentals/array1.js
@@ -0,0 +1,6 @@
+let marks=[20,25,37,45,60,75,85,90]
+console.log(marks)
+console.log(marks.length)
+console.log("element 1",marks[0])
+console.log("element 5",marks[4])
+console.log("last element",marks[7])
